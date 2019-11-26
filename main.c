65c1848ad3c647c9f2676c60fe639e8f8c7982fd
Hugo Baba <Hugobarette@gmail.com>
2019-11-26 12:58:33 +0100

    Ajout de commentaires

--- main.c
+++ main.c
@@ -36,7 +36,7 @@ int main(){
     int n_Player_try[NB_CHIFFRE];
     placement pla_Result[NB_CHIFFRE];
 
-    // Define variable
+    // Define variables
     int n_max_tries = 12;
     int n_tries_left;
 
@@ -82,7 +82,7 @@ int main(){
     return 0;
 }
 
-// Print the given array
+// Print the given array (not currently used in the code, may be usefull for debugging)
 void print_array(int *array){
     for(int i=0; i<NB_CHIFFRE; i++){
         printf("%d", array[i]);
@@ -90,7 +90,7 @@ void print_array(int *array){
     printf("\n");
 }
 
-// Initialize the asked result
+// Initialize the asked result with random integer between 0 and 9
 void initialize_result(int *n_Result){
     for(int i=0; i<NB_CHIFFRE; i++){
         n_Result[i] = rand() % 10;
@@ -108,13 +108,13 @@ void player_input(int *n_Player_try, int nombre){
 
             if((int)c == 8){ // If the player hit backspace
                 if(i > 0){
-                    // set the previous value to null, and go back to the previous iteration
+                    // set the previous value to null, and go back to the previous iteration, so the player can overwrite his previous input
                     i--;
                     n_Player_try[i] = (int)NULL;
                     i--;
                 }
                 printf("\b");
-            } else{ // Sinon
+            } else{ // If the player has entered anything else
                  n_Player_try[i] = c - 48;
                 if (n_Player_try[i] >= 0 || n_Player_try[i] <= 9){ // If the player hit a number between 0 and 9
                     printf("%d", n_Player_try[i]);
@@ -129,19 +129,24 @@ void player_input(int *n_Player_try, int nombre){
     printf("\n");
 }
 
-// Check the placement of each numbers in the player's try
+// Check the placement of each numbers in the player's try, and stock the results in the array pla_result
 void get_results(int *n_Player_try, int *n_Result, placement *pla_Result){
 
     for(int i = 0; i < NB_CHIFFRE; i++){
-        pla_Result[i] = Empty;
+        pla_Result[i] = Empty; // initialize the pla_result current cell at Empty
+
+        // If the current cell of the player inputs array is equal to the result array, stock wellPlaced in pla_result
         if(n_Player_try[i] == n_Result[i]){
             pla_Result[i] = wellPlaced;
         } else {
+            // In the other case, check if the number exist elsewhere in the array, and check for any double. Stock badly placed where appropriate.
             for(int j = 0; j < NB_CHIFFRE; j++){
                 if(n_Player_try[i] == n_Result[j] && n_Player_try[j] != n_Result[j]){
                     pla_Result[i] = badlyPlaced;
                 }
             }
+
+            // If the current cell result is neither wellPlaced and badlyPlaced, set it to wrong
             if(pla_Result[i] == Empty){
                 pla_Result[i] = wrong;
             }
@@ -149,7 +154,7 @@ void get_results(int *n_Player_try, int *n_Result, placement *pla_Result){
     }
 }
 
-// Check for the victory
+// Check for the victory, return TRUE if the player wins, FALSE if not
 int check_victory(placement *pla_Result){
     for(int i = 0; i < NB_CHIFFRE; i++){
         if(pla_Result[i] != wellPlaced){
@@ -162,16 +167,17 @@ int check_victory(placement *pla_Result){
 // Define colors
 void color(int t, int f){
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-        SetConsoleTextAttribute(hConsole,f*16+t);
+    SetConsoleTextAttribute(hConsole,f*16+t);
 }
 
-// Print the results in color
+// Print the results in color, and a review of the results, to help the player understand it
 void print_result(int *n_Player_try, placement *pla_Result){
 
     int n_wellPlaced_num = 0;
     int n_badlyPlaced_num = 0;
     int n_wrong_num = 0;
 
+    // Print the results itself in the write color, depending on the nature of its result (wellPlced, badlyPlaced or wrong)
     for(int i = 0; i < NB_CHIFFRE; i++){
 
         switch(pla_Result[i]){
@@ -189,6 +195,7 @@ void print_result(int *n_Player_try, placement *pla_Result){
         printf("%d", n_Player_try[i]);
     }
 
+    // Print the review of the results
     color(BLANC, NOIR);
     printf("\nVous avez ");
     color(BLANC, VERT);
@@ -199,6 +206,3 @@ void print_result(int *n_Player_try, placement *pla_Result){
     printf("%d nombre(s) faux\n", n_wrong_num);
     color(BLANC, NOIR);
 }
-
-
-
